90833433e3c61539f5f6c832a4763d48ea63749f
Paphop Sawasdee <paphop.staw@gmail.com>
2024-08-17 22:25:03 +0900

    Update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <footer className="flex flex-row items-center">Copyright</footer>;
+  return (
+    <footer className="flex flex-row justify-center p-2 sm:p-4 items-center">
+      © 2024 Stock Sawasdee
+    </footer>
+  );
 }

09269f69f9d15291bcf9f6396856ff0e1a330635
Paphop Sawasdee <paphop.staw@gmail.com>
2024-08-17 23:43:39 +0900

    Remove profile from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <Hero></Hero>
-      <Profile></Profile>
     </main>
   );
 }

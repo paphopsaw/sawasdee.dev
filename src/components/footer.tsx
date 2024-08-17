b7fdb913ecb4c39eb347b0c69fcd0bd21c12f856
Paphop Sawasdee <paphop.staw@gmail.com>
2024-08-18 01:00:51 +0900

    Remove hamburger nav

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="flex flex-row justify-center p-2 sm:p-4 items-center">
-      © 2024 Stock Sawasdee
+      © {new Date().getFullYear()} Stock Sawasdee
     </footer>
   );
 }

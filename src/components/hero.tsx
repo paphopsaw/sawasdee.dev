c2c863430f78165cde21c556f9a810d8504661c8
Paphop Sawasdee <paphop.staw@gmail.com>
2024-08-18 20:26:31 +0900

    Clean up

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,7 @@ export default function Hero() {
   const greeting = "Hello, I'm Stock Sawasdee.";
   const subtitle = "I'm a software engineer based in Yokohama, Japan.";
   return (
-    <section className="mb-10 sm:mb-12">
+    <section className="p-6 sm:p-10 mb-10 sm:mb-12">
       <h1 className="pb-4 sm:pb-8">{greeting}</h1>
       <h6>{subtitle}</h6>
     </section>
